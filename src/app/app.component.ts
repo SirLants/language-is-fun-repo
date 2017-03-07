43539bbb5bfce55f7861bc36416ecafca74dc611
Lants <lance.jo.johnson@gmail.com>
2017-03-06 20:01:27 -0600

    Added navbar and cleaned it up a bit. Started adding sidebar bits, my mistake for letting those sneak into this commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Language Is Fun Homepage!';
+  title = 'Language Is Fun!';
 }
