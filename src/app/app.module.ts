0d4a68ff6bf30557d05a69459226ab92abbe7a56
Lants <lance.jo.johnson@gmail.com>
2017-03-09 15:56:47 -0600

    Missed a typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     FormsModule,
     HttpModule,
     MaterialModule,
-    FlexLayoutModul
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
