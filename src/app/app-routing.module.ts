1d3491387d2eebc8993541d48ff3dc3b8b573bac
Lants <lance.jo.johnson@gmail.com>
2017-03-10 16:27:43 -0600

    Refactored main routing to occur in the app component and then nested the home routing inside that

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }								from '@angular/core';
 import { RouterModule, Routes }					from '@angular/router';
 
+import { HomeComponent }						from './home/home.component';
 import { HomeBodyComponent }					from './home-body/home-body.component';
 import { HomeMangaBrowserComponent }			from './home-manga-browser/home-manga-browser.component';
 import { UserProfileComponent }					from './user-profile/user-profile.component';
@@ -8,19 +9,19 @@ import { UserProfileSettingsComponent }			from './user-profile-settings/user-pro
 import { UserProfileStatsComponent }			from './user-profile-stats/user-profile-stats.component';
 import { UserProfileSocialComponent } 			from './user-profile-social/user-profile-social.component';
 
-import { QuizComponent }						from './quiz/quiz.component';
-
 const routes: Routes = [
-	{ path: '', 								redirectTo: '/home',	 							pathMatch: 'full' },
-	{ path: 'home', 		    				component: HomeBodyComponent },
-	{ path: 'home-manga-browser',    			component: HomeMangaBrowserComponent },
-	{ path: 'user-profile', 					component: UserProfileComponent, 					children: [
-		{path: '',								redirectTo: 'user-profile-settings',				pathMatch: 'full' },
-		{path: 'user-profile-settings',			component: UserProfileSettingsComponent},
-		{path: 'user-profile-stats',			component: UserProfileStatsComponent},
-		{path: 'user-profile-social',			component: UserProfileSocialComponent}
-	]},
-	{ path: '**',								redirectTo: '/home', 								pathMatch: 'full' }
+	{ path: '', 								redirectTo: 'home',	 							pathMatch: 'full' },
+	{ path: 'home',								component: HomeComponent, 						children: [
+		{ path: '',									redirectTo: 'home-body',							pathMatch: 'full' },
+		{ path: 'home-body', 		    			component: HomeBodyComponent },
+		{ path: 'home-manga-browser',    			component: HomeMangaBrowserComponent },
+		{ path: 'user-profile', 					component: UserProfileComponent, 					children: [
+			{path: '',									redirectTo: 'user-profile-settings',				pathMatch: 'full' },
+			{path: 'user-profile-settings',				component: UserProfileSettingsComponent},
+			{path: 'user-profile-stats',				component: UserProfileStatsComponent},
+			{path: 'user-profile-social',				component: UserProfileSocialComponent}
+		]}
+	]}
 ];
 
 @NgModule({
