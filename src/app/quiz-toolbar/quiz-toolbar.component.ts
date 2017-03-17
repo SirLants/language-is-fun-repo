08d745b06385ddfe182ce10a80736697ac000c6e
Lants <lance.jo.johnson@gmail.com>
2017-03-17 12:37:48 -0500

    Cleanup on a lot of the screens. I added some dialog service functionality to one portion but I mostly started cleaning up a lot of the styles

--- src/app/quiz-toolbar/quiz-toolbar.component.ts
+++ src/app/quiz-toolbar/quiz-toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }    from '@angular/core';
+import { DialogsService }       from "../services/dialogs.service";
 
 @Component({
   selector: 'quiz-toolbar',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz-toolbar.component.css']
 })
 export class QuizToolbarComponent implements OnInit {
+  public result:any;
 
-  constructor() { }
+  constructor(private dialogsService: DialogsService) { }
 
+  public openGoHome() {
+    this.dialogsService
+      .goHome()
+      .subscribe(res => this.result = res);
+  }
   ngOnInit() {
   }
 
